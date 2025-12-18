83b2d8e17b9a0d92cd0e9056f14350b7d49b7c89
Ashish Singh <ashish.singh@eminds.ai>
2025-12-18 09:08:53 +0530

    test: test for embeddings and code graph features

---
+++ processors.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+type DataHandler struct {
+	Name string
+}
+
+func (d DataHandler) Process(ctx interface{}, data interface{}) (string, error) {
+	return fmt.Sprintf("processed: %v", data), nil
+}
+
+type Cache struct {
+	data map[string]interface{}
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.data[key]
+}
+
+func (c *Cache) Set(key string, value interface{}, ttl int) {
+	if c.data == nil {
+		c.data = make(map[string]interface{})
+	}
+	c.data[key] = value
+}
+
+func (c *Cache) Delete(key string) int {
+	delete(c.data, key)
+	return 0
+}
+
+type Logger struct {
+	prefix string
+}
+
+func (l Logger) Info(msg string, args interface{}) {
+	fmt.Printf("[INFO] %s: %s %v\n", l.prefix, msg, args)
+}
+
+func (l Logger) Error(msg string, args interface{}) {
+	fmt.Printf("[ERROR] %s: %s %v\n", l.prefix, msg, args)
+}
+
+func (l Logger) Debug(msg string, args interface{}) {
+	fmt.Printf("[DEBUG] %s: %s %v\n", l.prefix, msg, args)
+}
+
+func (l Logger) Warn(msg string, args interface{}) {
+	fmt.Printf("[WARN] %s: %s %v\n", l.prefix, msg, args)
+}
+
+func TransformData(input interface{}) (int, error) {
+	return 42, nil
+}
+
+func FilterData(data []interface{}) string {
+	return "filtered"
+}
+
+type Transformer struct {
+	config string
+}
+
+func (t Transformer) Transform(input interface{}) (interface{}, int) {
+	return input, 0
+}
+
