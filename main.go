692cd1d758dcc17c1892c40f7735ae29b616606c
Ashish Singh <ashish.singh@eminds.ai>
2026-01-05 20:24:58 +0530

    test uses_type rel

--- main.go
+++ main.go
@@ -21,18 +21,20 @@ func main() {
 	calc := &utils.Calculator{}
 	mulResult := calc.Multiply(10, 5)
 	fmt.Printf("Multiplied Result: %d\n", mulResult)
-
 	var op utils.Operator = calc
 	interfaceResult := op.Multiply(6, 7)
 	fmt.Printf("Interface Result: %d\n", interfaceResult)
+
 	var num utils.Number = 100
 	fmt.Printf("Number type: %d\n", num)
 
 	fmt.Printf("Max Iterations: %d\n", utils.MaxIterations)
 	fmt.Printf("Pi: %.5f\n", utils.Pi)
-
 	globalCounter = 42
 	fmt.Printf("App: %s, Counter: %d\n", appName, globalCounter)
+
+	squared := utils.Square(8)
+	fmt.Printf("Square of 8: %d\n", squared)
 }
 
 
