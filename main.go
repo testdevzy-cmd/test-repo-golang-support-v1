23de0c0638e9c1d3aff628c2168a0f518283acfe
Ashish Singh <ashish@devzy.ai>
2026-05-26 15:06:02 +0530

    test: review summary table

--- main.go
+++ main.go
@@ -6,13 +6,35 @@ import (
 	"github.com/test-repo/test-repo-golang-support-v1/utils"
 )
 
+var (
+	globalCounter int
+	appName       string = "GraphTester"
+)
+
 func main() {
 	result := utils.Add(10, 5)
 	fmt.Printf("Initial Result: %d\n", result)
-	
-	diff := utils.Subtract(10, 5)
-	fmt.Printf("Difference: %d\n", diff)
 
-	quotient := utils.Divide(10, 5)
-	fmt.Printf("Quotient: %d\n", quotient)
+	subResult := utils.Subtract(10, 5)
+	fmt.Printf("Subtracted Result: %d\n", subResult)
+
+	calc := &utils.Calculator{}
+	mulResult := calc.Multiply(10, 5)
+	fmt.Printf("Multiplied Result: %d\n", mulResult)
+	var op utils.Operator = calc
+	interfaceResult := op.Multiply(6, 7)
+	fmt.Printf("Interface Result: %d\n", interfaceResult)
+
+	var num utils.Number = 100
+	fmt.Printf("Number type: %d\n", num)
+
+	fmt.Printf("Max Iterations: %d\n", utils.MaxIterations)
+	fmt.Printf("Pi: %.5f\n", utils.Pi)
+	globalCounter = 42
+	fmt.Printf("App: %s, Counter: %d\n", appName, globalCounter)
+
+	squared := utils.Square(8)
+	fmt.Printf("Square of 8: %d\n", squared)
 }
+
+
--- utils/math.go
+++ utils/math.go
@@ -8,13 +8,3 @@ func Subtract(a, b int) int {
 	return a - b
 }
 
-func Multiply(a, b int) int {
-	return a * b
-}
-
-func Divide(a, b int) int {
-	if b == 0 {
-		return 0
-	}
-	return a / b
-}
